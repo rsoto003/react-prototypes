9ff3543a257e75a52fcb741185d648b90a5a68ce
Ryan Soto <rsoto003@ucr.edu>
2018-03-16 15:08:51 -0700

    react-intro finished, pushing changes.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+const user = {
+    name: 'ryan',
+    luckyNumber: luckyNumber()
+}
+
+function luckyNumber(){
+    return parseInt(Math.random() * 1000);
+}
+
+function greeting(obj){
+    return (
+        <div class="container">
+            <h1>Hello, {obj.name}!</h1>
+            <h2 class="text-muted">Your lucky number is: {obj.luckyNumber}</h2>
+        </div>
+    )
+}
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
